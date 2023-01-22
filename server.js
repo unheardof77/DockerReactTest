8cb34ff50ab12ab3aa9defc2d92bf97dd093eb10
Morgan Tolman <morgan.tolman04@gmail.com>
2023-01-22 11:58:18 -0700

    Added a server to serve react file

---
+++ server.js
@@ -0,0 +1,14 @@
+import express from "express";
+import path from "path";
+const PORT = process.env.PORT || 3000;
+const app = express();
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, "/react-client/build")));
+    app.get("/", (req, res)=>{
+        res.sendFile(path.join(__dirname, "/react-client/build/index.html"))
+    });
+}
+
+app.listen(PORT, ()=>{
+    console.log(`Listening on port ${PORT}`);
+});
