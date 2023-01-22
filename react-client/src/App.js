639da7a48e9dc0a9c5b3359ffb391b402565ebe7
Morgan Tolman <morgan.tolman04@gmail.com>
2023-01-21 19:21:36 -0700

    testing again

--- react-client/src/App.js
+++ react-client/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reloadjkd.
+          Edit <code>src/App.js</code> and sddsave to reloadjkd.
         </p>
         <a
           className="App-link"
